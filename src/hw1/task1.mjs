8f79b4dd9f4f0857e10d84d345053f0f7e696f6c
Serhii <sfrok99@gmail.com>
2021-07-26 19:44:47 +0300

    feat(task1): add task1

---
+++ src/hw1/task1.mjs
@@ -0,0 +1,31 @@
+// Validator
+import { PaintCansValidator } from "./validator.mjs";
+
+function countPaintCans(length, width, height) {
+  try {
+    const UPPER_BOUND = 1000;
+    const CONSUMPTION = 16;
+    PaintCansValidator.isInteger(length)
+      .isInteger(width)
+      .isInteger(height)
+      .lowerThanZeroStrictly(length)
+      .lowerThanZeroStrictly(width)
+      .lowerThanZeroStrictly(height)
+      .lowerOrEqual(length, UPPER_BOUND)
+      .lowerOrEqual(width, UPPER_BOUND)
+      .lowerOrEqual(height, UPPER_BOUND);
+
+    // hw + hl + hw + hl = 2hw + 2hl = 2(hw + hl)
+    const square = 2 * (height * width + height * length);
+    return square % CONSUMPTION !== 0
+      ? Math.ceil(square / CONSUMPTION)
+      : square / CONSUMPTION;
+  } catch {
+    return "check arguments";
+  }
+}
+
+console.log(countPaintCans(100.5, 400, 300)); // Incorrect call
+console.log(countPaintCans(100, -400, 300)); // Incorrect call
+console.log(countPaintCans(1001, 400, 300)); // Incorrect call
+console.log(countPaintCans(500, 400, 300)); // OK
---
+++ src/hw1/validator.mjs
@@ -0,0 +1,68 @@
+class Validator {
+  static isInteger(number) {
+    return Number.isInteger(number);
+  }
+
+  static isNumber(entity) {
+    return Number.isFinite(entity);
+  }
+
+  static greaterThanZeroStrictly(number) {
+    return number > 0;
+  }
+
+  static lowerThanZeroStrictly(number) {
+    return number < 0;
+  }
+
+  static greaterOrEqualZero(number) {
+    return number >= 0;
+  }
+
+  static lowerOrEqualZero(number) {
+    return number <= 0;
+  }
+
+  static greaterStrictly(number, expectedLength) {
+    return number > expectedLength;
+  }
+
+  static lowerStrictly(number, expectedLength) {
+    return number < expectedLength;
+  }
+
+  static greaterOrEqual(number, expectedLength) {
+    return number >= expectedLength;
+  }
+
+  static lowerOrEqual(number, expectedLength) {
+    return number <= expectedLength;
+  }
+
+  static checkArgumentsAmount(args, expectedAmount) {
+    return args.length > expectedAmount;
+  }
+}
+
+export class PaintCansValidator extends Validator {
+  static isInteger(number) {
+    if (!super.isInteger(number)) {
+      throw Error("incorrect arguments");
+    }
+    return this;
+  }
+
+  static lowerThanZeroStrictly(number) {
+    if (super.lowerThanZeroStrictly(number)) {
+      throw Error("incorrect arguments");
+    }
+    return this;
+  }
+
+  static lowerOrEqual(number, expectedLength) {
+    if (!super.lowerOrEqual(number, expectedLength)) {
+      throw Error("incorrect arguments");
+    }
+    return this;
+  }
+}
