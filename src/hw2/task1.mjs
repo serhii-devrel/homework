3bd893594910f9e3c5abb46ead0c1536f7b0c353
Serhii <sfrok99@gmail.com>
2021-07-27 19:55:50 +0300

    feat(hw2): add validator

--- src/hw2/task1.mjs
+++ src/hw2/task1.mjs
@@ -1,6 +1,26 @@
+// Validator
+import { SumMinimumsValidator } from "./validator.mjs";
+
 function sumOfMinimums(arr) {
-  return arr.map((item) => Math.min(...item)).reduce((a, b) => a + b, 0);
+  try {
+    SumMinimumsValidator.isArray(arr)
+      .arrayNotEmpty(arr)
+      .arrayContainsOnlyIntegers(arr);
+
+    return arr.map((item) => Math.min(...item)).reduce((a, b) => a + b, 0);
+  } catch {
+    return "check arguments";
+  }
 }
+
+console.log(sumOfMinimums([])); // Incorrect call
+console.log(
+  sumOfMinimums([
+    [-1, -2, 3, 4, 5],
+    [5, 6, 7, 8, 9],
+    [20, 21, 34, 56, 100],
+  ])
+); // Incorrect call
 
 console.log(
   sumOfMinimums([
--- src/hw2/task2.mjs
+++ src/hw2/task2.mjs
@@ -1,9 +1,19 @@
-function twoOldestAges(arr) {
-  return arr
-    .sort((a, b) => b - a)
-    .slice(0, 2)
-    .reverse();
-}
+// Validator
+import { OldestAgesValidator } from "./validator.mjs";
 
-console.log(twoOldestAges([2, 5, 3])); // OK
+function twoOldestAges(arr) {
+  try {
+    const UPPER_BOUND = 2;
+    OldestAgesValidator.isArray(arr).arrayGreaterOrEqual(arr, UPPER_BOUND);
+
+    return arr
+      .sort((a, b) => b - a)
+      .slice(0, 2)
+      .reverse();
+  } catch {
+    return "check arguments";
+  }
+}
+console.log(twoOldestAges([])); // Incorrect call
+console.log(twoOldestAges([2, 5])); // OK
 console.log(twoOldestAges([1, 2, 10, 8])); // OK
