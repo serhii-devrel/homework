aa901207e22a3b71761061555a58625ab5deb35d
Serhii <sfrok99@gmail.com>
2021-07-29 23:19:59 +0300

    update(task2): update solution

--- src/hw4/task2.mjs
+++ src/hw4/task2.mjs
@@ -1,9 +1,23 @@
 // Validator
 import { MorseValidator } from "./validator.mjs";
 
+// Morse table
+import { MORSE_TABLE } from "./morse.mjs";
+
 function decodeMorse(data) {
   try {
     MorseValidator.isString(data).stringNotEmpty(data);
+
+    return data
+      .trim()
+      .split("   ")
+      .map((i) =>
+        i
+          .split(" ")
+          .map((i) => MORSE_TABLE[i])
+          .join("")
+      )
+      .join(" ");
   } catch {
     return "check arguments";
   }
