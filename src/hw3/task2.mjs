5cdf07923372d65ad68801f4fb7c0d068e2b717c
Serhii <sfrok99@gmail.com>
2021-07-29 10:00:57 +0300

    update(task2): update solution

--- src/hw3/task2.mjs
+++ src/hw3/task2.mjs
@@ -4,6 +4,30 @@ import { TicTacToeValidator } from "./validator.mjs";
 function checkTicTacToe(desk) {
   try {
     TicTacToeValidator.isArray(desk);
+
+    const TYPE = {
+      X: 3,
+      O: 30,
+      NOT_FINISHED: 0,
+    };
+    const sumOf2D = (arr) => arr.map((i) => i.reduce((a, b) => a + b, 0));
+    const replacedDesk = desk.map((i) => i.map((i) => (i === 2 ? 10 : i)));
+    const horizontal = sumOf2D(replacedDesk);
+    const deskT = replacedDesk[0].map((_, i) => replacedDesk.map((r) => r[i]));
+    const vertical = sumOf2D(deskT);
+    const mainDiagonal = sumOf2D([replacedDesk.map((i, index) => i[index])]);
+    const sideDiagonal = sumOf2D([replacedDesk.map((r, i) => r[-i + 2])]);
+    const SELECTION = [horizontal, vertical, mainDiagonal, sideDiagonal].flat();
+    switch (true) {
+      case SELECTION.includes(TYPE.X):
+        return "X";
+      case SELECTION.includes(TYPE.O):
+        return "O";
+      case replacedDesk.flat().includes(TYPE.NOT_FINISHED):
+        return -1;
+      default:
+        return 0;
+    }
   } catch {
     return "check arguments";
   }
