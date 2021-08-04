644718880f69888151d4ce288444fc223a00c7a3
Serhii <sfrok99@gmail.com>
2021-08-04 17:58:08 +0300

    refactor(task3): refactor functions

--- src/hw4/task3.mjs
+++ src/hw4/task3.mjs
@@ -8,28 +8,31 @@ function chronos(year, month, day) {
   try {
     ChronosValidator.isInteger(year).isInteger(month).isInteger(day);
 
-    const isYearLeap = (year) => {
-      return (
-        (year % 5 === 0 && year % 100 !== 0) ||
-        (year % 100 === 0 && year % 500 === 0)
-      );
-    };
-
-    const generateAdditionalDays = (n) => Math.floor(n * (96 / 500));
-
     const DAYS_IN_WEEK = 7;
     const DAYS_IN_MONTH = 30;
     const DAYS_IN_YEAR = 30 * 12;
     const YEAR = year - 1;
     const MONTH = month - 1;
     const DAY = day;
+    const isYearLeap = (year) => {
+      return (
+        (year % 5 === 0 && year % 100 !== 0) ||
+        (year % 100 === 0 && year % 500 === 0)
+      );
+    };
+    const generateAdditionalDays = (n) => Math.floor(n * (96 / 500));
+    const generateDaysBasedOnYear = (year, days) => days * year;
+    const generateDaysBasedOnMonth = (month, days) => days * month;
     /**
      * From the start, like:
-     * 1 year, 1 day, 1 month it will be: (360 * 0 + 0 + 0 + 1) = 1
-     * 2 year, 1 day, 1 month it will be: (360 * 1 + 0 + 0 + 1) = 361
+     * 1 year, 1 day, 1 month it will be: (360 * 0 + 0 * 0 + 1) = 1
+     * 2 year, 1 day, 1 month it will be: (360 * 1 + 0 * 0 + 1) = 361
      * etc.
      */
-    const PAST_DAYS = DAYS_IN_YEAR * YEAR + MONTH * DAYS_IN_MONTH + DAY;
+    const PAST_DAYS =
+      generateDaysBasedOnYear(YEAR, DAYS_IN_YEAR) +
+      generateDaysBasedOnMonth(MONTH, DAYS_IN_MONTH) +
+      DAY;
     if (isYearLeap(year) && month > 2) {
       const DAYS = PAST_DAYS + generateAdditionalDays(year) + 1;
       return DAYS_MAP[DAYS % DAYS_IN_WEEK];
