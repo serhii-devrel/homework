5466fd21953107d55c0c9460aa0b59d7c3e21181
Serhii <sfrok99@gmail.com>
2021-08-07 13:30:47 +0300

    update(task1): update solution

--- src/hw5/task1.mjs
+++ src/hw5/task1.mjs
@@ -1,9 +1,24 @@
 // Validator
 import { QuipuValidator } from "./validator.mjs";
 
+// Helpers
+import {
+  getParts,
+  makeQuipuShape,
+  defineOperationWithNumbers,
+} from "./helpers.mjs";
+
 function quipuCalculator(expression) {
   try {
     QuipuValidator.isString(expression).stringNotEmpty(expression);
+
+    const arrange = [];
+    for (let i = 0; i < expression.length; i += 1) {
+      const [c, p, item] = getParts(expression, i, arrange);
+      defineOperationWithNumbers(c, p, item, arrange);
+    }
+    const stringBasedOnData = arrange.join("");
+    return makeQuipuShape(String(eval(stringBasedOnData))).slice(0, -1);
   } catch {
     return "check arguments";
   }
